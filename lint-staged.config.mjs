d8f3a333c17d62848ec8f40d1aadec86b9120a1d
nihil-pro <Romanolga13>
2025-01-21 20:32:45 +0300

    prepare

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * @docs: https://github.com/okonet/lint-staged
+ *
+ * Runs commands for files added to commit
+ *
+ */
+export default {
+  '(*.js|*.ts|*.cjs|*.mjs)': ['npm run format:js'],
+};
