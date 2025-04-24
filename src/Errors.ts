379a69af0f978e273e5ec0e234e234d6e2d37ddc
nihil-pro <Romanolga13>
2025-04-24 19:37:57 +0300

    fix bugs

--- src/Errors.ts
+++
@@ -1,7 +0,0 @@
-export class TransformerError extends Error { }
-
-export const INVALID = {
-  TARGET: (path: string[]) => `Target class "${path.join('.')}" can't be invoked with "new"`,
-  JSON: (path: string[]) => `Source can't be converted to target class "${path.join('.')}"`,
-  TYPE: (key: string, path: string[]) => `Value of "${key}" in source, can't be converted to type of "${path.join('.')}"`,
-}
